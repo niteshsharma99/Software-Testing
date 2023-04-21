a06a6183255de086a639ce8abda796ef51b3df11
Nitesh Sharma <75922520+niteshsharma99@users.noreply.github.com>
2023-04-21 09:53:09 +0530

    Update InvokeOtherBrowsers.java

--- SeleniumProject1/src/com/selenium/basic/InvokeOtherBrowsers.java
+++ SeleniumProject1/src/com/selenium/basic/InvokeOtherBrowsers.java
@@ -7,32 +7,25 @@ import org.testng.annotations.Test;
 
 public class InvokeOtherBrowsers {
 	
-
 	@Test
 	public void verifyFaceBookHomePage(){
 		
 		String URL = "https://www.facebook.com";
 		
-/*//		//Open the URL with Selenium
-		System.setProperty("webdriver.chrome.driver", "C:\Users\nkumarsharma\eclipse-workspace\SeleniumProject1\Drivers\chromedriver.exe");
+		//Open the URL with Selenium
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\nkumarsharma\\eclipse-workspace\\SeleniumProject1\\Drivers\\chromedriver.exe");
 		
-//		//Only for Windpws
-//		//System.setProperty("webdriver.chrome.driver", "c:\\test\\selenium.....\\chromedriver.exe")
 		
-		WebDriver driver = new ChromeDriver();*/
+		WebDriver driver = new ChromeDriver();
 		
 		//FireFox Browser Invoke
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\nkumarsharma\\eclipse-workspace\\SeleniumProject1\\Drivers\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver1 = new FirefoxDriver();
 		
-		/*System.setProperty("webdriver.opera.driver", "");
-		WebDriver driver = new OperaDriver();*/
 		
-	/*	System.setProperty("webdriver.ie.driver", "PATH IE Driver EXE");
-		WebDriver driver = new InternetExplorerDriver();*/
 		
-//      System.setProperty("webdriver.edge.driver", "C:\\Users\\nkumarsharma\\eclipse-workspace\\SeleniumProject1\\Drivers\\msedgedriver.exe");    
-//      WebDriver driver = new EdgeDriver();
+      System.setProperty("webdriver.edge.driver", "C:\\Users\\nkumarsharma\\eclipse-workspace\\SeleniumProject1\\Drivers\\msedgedriver.exe");    
+      WebDriver driver3 = new EdgeDriver();
 		
 		//To maximize the Browser
 		driver.manage().window().maximize();
